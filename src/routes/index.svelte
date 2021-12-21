ef2c0ab2c53e1e832e074707b8bebacff1c4903d
Vjet Node JS <hoaboy89@gmail.com>
2021-12-21 08:02:06 +0000

    Complete develop HTML structure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,16 @@
-<h1>Todos</h1>
+<script>
+    import TodoItem from "$lib/todo-item.svelte";
+    const title= "Todo"
+</script>
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+
+<h1>{title}</h1>
 <form action="" method="">
     <input type="text" name="text" aria-label="Add to do" placeholder="+ Add to list" id="">
 </form>
+<TodoItem/>
+<TodoItem/>
+<TodoItem/>
