9d47df2ae4ef3cb9a73e271918082f661caf4c71
Vjet Node JS <hoaboy89@gmail.com>
2021-12-23 04:12:20 +0000

    Complete PATCH update text

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -73,8 +73,8 @@
         <input type="hidden" name="done" value="" />
         <button aria-label="Mark done/not done" class="toggle"></button>
     </form>
-    <form action="" method="" class="text">
-        <input type="text" value="{todo.text}"/>
+    <form action="/todos/{todo.uid}.json?_method=patch" method="post" class="text">
+        <input type="text" name="text" value="{todo.text}" />
         <button aria-label="Save todo" class="save"></button>
     </form>
     <form action="/todos/{todo.uid}.json?_method=delete" method="post">
--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -3,7 +3,7 @@ import type { Request } from "@sveltejs/kit";
 //TODO: INSERT FROM DATABASE
 let todos: Todo[]= [];
 
-export const api=(request: Request, todo?: Todo)=>{
+export const api = (request: Request, data?: Record<string, unknown>) => {
     let body= {};
     let status= 500;
     //request.query.get("")
@@ -14,13 +14,22 @@ export const api=(request: Request, todo?: Todo)=>{
             status= 200;
             break;
         case "POST":
-            todos.push(todo);
-            body= todo;
+            todos.push(data as Todo);
+            body = data;
             status= 201;
             break;
         case "DELETE":
             todos= todos.filter(todo => todo.uid !== request.params.uid)
             status= 200;
+            break;
+        case "PATCH":
+            todos = todos.map(todo => {
+                if (todo.uid === request.params.uid) {
+                todo.text = data.text as string;
+                }
+                 return todo;
+            });
+                status = 200;
             break;
         default:
             break;
